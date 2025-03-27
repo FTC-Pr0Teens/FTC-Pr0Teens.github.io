ab545c9268623b3b4ea65bdd4befae56108226e4
Kaitlyn <kaitlyn.chuchu@gmail.com>
2025-03-27 19:57:16 -0400

    f

--- HTML/javascript/script.js
+++ HTML/javascript/script.js
@@ -16,16 +16,3 @@ function nextSlide() {
     index = (index < totalImages - 1) ? index + 1 : 0;
     showSlide();
 }
-window.onscroll = function() {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  var navbar = document.getElementById("navbar");
-
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      navbar.style.top = "0";
-  } else {
-      navbar.style.top = "-50px";
-  }
-}
